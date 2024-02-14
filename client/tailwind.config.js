cdee19a9d8d7be0215a6a42e3c9690d63116f76f
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-02-13 23:43:26 -0500

    Update CSS and Tailwind configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,js,ts,jsx,tsx}"];
+export const purge = ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'];
+export const darkMode = false;
 export const theme = {
   extend: {},
 };
+export const variants = {
+  extend: {},
+};
 export const plugins = [];
-
